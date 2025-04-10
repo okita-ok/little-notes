37e179326ee57f3f268f7dd91b5c32e13daffeb9
okita <109244075+okita-ok@users.noreply.github.com>
2025-04-09 22:23:47 -0400

    API: melhorias no mongoose

--- api/src/plugins/mongoose.ts
+++ api/src/plugins/mongoose.ts
@@ -4,10 +4,30 @@ import { FastifyInstance } from "fastify";
 
 const connectDB = async (fastify: FastifyInstance) => {
   try {
-    await mongoose.connect("mongodb://mongo:27017/notes");
+    await mongoose.connect(process.env.MONGO_URI!, {
+      serverSelectionTimeoutMS: 5000, // 5s de timeout
+      heartbeatFrequencyMS: 5000, // 5s de freq para verificacao
+    });
+
+    mongoose.connection.on("error", (err) => {
+      fastify.log.error("🔴 Erro no Mongoose após conectar:", err);
+      console.error("🔴 Erro no Mongoose após conectar:", err);
+      process.exit(1);
+    });
+    mongoose.connection.on("disconnected", () => {
+      fastify.log.warn("⚠️ Mongoose foi desconectado do banco");
+      console.log("⚠️ Mongoose foi desconectado do banco");
+    });
+    mongoose.connection.on("reconnected", () => {
+      fastify.log.info("🔁 Mongoose reconectado.");
+      console.log("🔁 Mongoose reconectado.");
+    });
+
+    fastify.log.info("🟢 Mongoose conectado com sucesso");
     console.log("🟢 Mongoose conectado com sucesso");
   } catch (err) {
-    console.error("🔴 Erro ao conectar o mongoose:", err);
+    fastify.log.error("🔴 Erro ao conectar com o mongoose:", err);
+    console.error("🔴 Erro ao conectar com o mongoose:", err);
     process.exit(1);
   }
 };
